f5a47b7ad8c371d7486e4867ecfb11412d29c8e3
jurako <jurako91@gmail.com>
2016-11-17 12:38:32 +0200

    Add files via upload

--- views/contacts/index.php
+++ views/contacts/index.php
@@ -19,7 +19,7 @@
                     Ваш вопрос, отзыв или пожелание:
                 </div>
                 <div style="width: 100%; height: auto; text-align: center; font-family: Arial, Helvetica, sans-serif; color: #797878; line-height: 1.8;">
-                    <form action="/OnlineShop/contact" method="post">
+                    <form action="/contact" method="post">
                         <textarea cols="50" rows="8" name="feedback"><?php echo $feedback; ?></textarea>
                         <div style="margin-top: 15px;">Адрес для получения ответа:</div>
                         <input type="text" value="<?php if (isset($_SESSION['userId'])) echo $user['E_mail']; ?>" name="email">
@@ -38,7 +38,7 @@
                         <li>
                             <a class="title" href="catalog/<?php echo $product['categoryName'].'/'.$product['id']?>"><?php echo $product['name']; ?></a>
                             <a class="img" href="catalog/<?php echo $product['categoryName'].'/'.$product['id']?>">
-                                <img src="http://localhost/OnlineShop/templates/images/products/productID_<?php echo $product['id'] . '_small'?>.jpg" alt="<?php echo $product['name']; ?>">
+                                <img src="/templates/images/products/productID_<?php echo $product['id'] . '_small'?>.jpg" alt="<?php echo $product['name']; ?>">
                             </a>
                             <p class="price"><?php echo $product['price']; ?> €</p>
                         </li>
