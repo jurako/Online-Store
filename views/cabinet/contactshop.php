a5fb5331fec7fd55b295f5e48c5db1e5ab3e558c
jurako <jurako91@gmail.com>
2016-11-17 12:37:46 +0200

    Add files via upload

--- views/cabinet/contactshop.php
+++ views/cabinet/contactshop.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="UTF-8">
         <title>Личный кабинет</title>
-        <link rel="stylesheet" type="text/css" href="http://localhost/OnlineShop/templates/css/main-style.css">
-        <link rel="stylesheet" type="text/css" href="http://localhost/OnlineShop/templates/css/filters.css">
-        <link rel="stylesheet" type="text/css" href="http://localhost/OnlineShop/templates/css/cabinet.css">
+        <link rel="stylesheet" type="text/css" href="/templates/css/main-style.css">
+        <link rel="stylesheet" type="text/css" href="/templates/css/filters.css">
+        <link rel="stylesheet" type="text/css" href="/templates/css/cabinet.css">
         <style>
             body {
-                background-image: url(http://localhost/OnlineShop/templates/images/body_bg_image.png);
+                background-image: url(/templates/images/body_bg_image.png);
                 background-position: center left;
                 background-repeat: repeat;
                 background-attachment: scroll;
@@ -25,16 +25,16 @@
             <div id="middle" style="background-color: white; margin-top: 180px; padding-top: 5px; padding-bottom: 5px;">
                 <div class="options-form">
                     <div class="links fl">
-                        <a href="/OnlineShop/"><span style="font-weight: bold;">Вернуться в магазин</span></a><span style="margin: 0 7px;">|</span>
-                        <a href="/OnlineShop/cabinet/contactinfo">Контактные данные</span></a><span style="margin: 0 7px;">|</span>
-                        <a href="/OnlineShop/cabinet/orders">Заказы</a><span style="margin: 0 7px;">|</span>
-                        <a href="/OnlineShop/cabinet/discounts">Скидки</a><span style="margin: 0 7px;">|</span>
-                        <a href="<?php echo $_SERVER["REQUEST_URI"]; ?>"><span style="font-weight: bold;">Обратная связь</span></a>
+                        <a href="/"><span style="font-weight: bold;">Вернуться в магазин</span></a><span style="margin: 0 7px;">|</span>
+                        <a href="/cabinet/contactinfo">Контактные данные</span></a><span style="margin: 0 7px;">|</span>
+                        <a href="/cabinet/orders">Заказы</a><span style="margin: 0 7px;">|</span>
+                        <a href="/cabinet/discounts">Скидки</a><span style="margin: 0 7px;">|</span>
+                        <a href="<?php echo htmlentities($_SERVER["REQUEST_URI"]); ?>"><span style="font-weight: bold;">Обратная связь</span></a>
                     </div>
                     <div class="logout-div fr">
                         <?php echo $user['Name']; ?>
                         (
-                        <a href="/OnlineShop/user/logout" class="links">Выход</a>
+                        <a href="/user/logout" class="links">Выход</a>
                         )
                     </div>
                 </div>
@@ -57,7 +57,7 @@
                         Ваш вопрос, отзыв или пожелание:
                     </div>
                     <div style="width: 100%; height: auto; text-align: center; font-family: Arial, Helvetica, sans-serif; color: #797878; line-height: 1.8;">
-                        <form action="/OnlineShop/cabinet/contact" method="post">
+                        <form action="/cabinet/contact" method="post">
                             <textarea cols="50" rows="8" name="feedback"><?php echo $feedback; ?></textarea>
                             <div style="margin-top: 15px;">Адрес для получения ответа:</div>
                             <input type="text" value="<?php if (isset($_SESSION['userId'])) echo $user['E_mail']; ?>" name="email">
--- views/cabinet/orders.php
+++ views/cabinet/orders.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="UTF-8">
         <title>Личный кабинет</title>
-        <link rel="stylesheet" type="text/css" href="http://localhost/OnlineShop/templates/css/main-style.css">
-        <link rel="stylesheet" type="text/css" href="http://localhost/OnlineShop/templates/css/filters.css">
-        <link rel="stylesheet" type="text/css" href="http://localhost/OnlineShop/templates/css/cabinet.css">
+        <link rel="stylesheet" type="text/css" href="/templates/css/main-style.css">
+        <link rel="stylesheet" type="text/css" href="/templates/css/filters.css">
+        <link rel="stylesheet" type="text/css" href="/templates/css/cabinet.css">
         <style>
             body {
-                background-image: url(http://localhost/OnlineShop/templates/images/body_bg_image.png);
+                background-image: url(/templates/images/body_bg_image.png);
                 background-position: center left;
                 background-repeat: repeat;
                 background-attachment: scroll;
@@ -25,16 +25,16 @@
             <div id="middle" style="background-color: white; margin-top: 180px; padding-top: 5px; padding-bottom: 5px;">
                 <div class="options-form">
                     <div class="links fl">
-                        <a href="/OnlineShop/"><span style="font-weight: bold;">Вернуться в магазин</span></a><span style="margin: 0 7px;">|</span>
-                        <a href="/OnlineShop/cabinet/contactinfo">Контактные данные</a><span style="margin: 0 7px;">|</span>
-                        <a href="<?php echo $_SERVER["REQUEST_URI"]; ?>"><span style="font-weight: bold;">Заказы</span></a><span style="margin: 0 7px;">|</span>
-                        <a href="/OnlineShop/cabinet/discounts">Скидки</a><span style="margin: 0 7px;">|</span>
-                        <a href="/OnlineShop/cabinet/contactshop">Обратная связь</a>
+                        <a href="/"><span style="font-weight: bold;">Вернуться в магазин</span></a><span style="margin: 0 7px;">|</span>
+                        <a href="/cabinet/contactinfo">Контактные данные</a><span style="margin: 0 7px;">|</span>
+                        <a href="<?php echo htmlentities($_SERVER["REQUEST_URI"]); ?>"><span style="font-weight: bold;">Заказы</span></a><span style="margin: 0 7px;">|</span>
+                        <a href="/cabinet/discounts">Скидки</a><span style="margin: 0 7px;">|</span>
+                        <a href="/cabinet/contactshop">Обратная связь</a>
                     </div>
                     <div class="logout-div fr">
                         <?php echo $user['Name']; ?>
                         (
-                        <a href="/OnlineShop/user/logout" class="links">Выход</a>
+                        <a href="/user/logout" class="links">Выход</a>
                         )
                     </div>
                 </div>
@@ -67,16 +67,16 @@
                                     <?php foreach ($orders as $order) { ?>
                                         <tr>    
                                             <td style="width: 180px;">
-                                                <a class="table-row" style="text-decoration: underline;" href="/OnlineShop/cabinet/orders/<?php echo $order['order_id']; ?>"> <?php echo $order['order_id']; ?></a>
+                                                <a class="table-row" style="text-decoration: underline;" href="/cabinet/orders/<?php echo $order['order_id']; ?>"> <?php echo $order['order_id']; ?></a>
                                             </td>
                                             <td style="width: 342px;">
-                                                <a class="table-row" style="text-decoration: underline;" href="/OnlineShop/cabinet/orders/<?php echo $order['order_id']; ?>"> <?php echo $order['order_date']; ?></a>
+                                                <a class="table-row" style="text-decoration: underline;" href="/cabinet/orders/<?php echo $order['order_id']; ?>"> <?php echo $order['order_date']; ?></a>
                                             </td>
                                             <td style="width: 185px;">
-                                                <a class="table-row" style="text-decoration: underline;" href="/OnlineShop/cabinet/orders/<?php echo $order['order_id']; ?>"> <span <?php if ($order['status'] == 0) echo 'style="background: #F3C7C7; color: #C7271F; padding: 3px;">не оплачен'; else echo 'style="background: #8FFF87; color: #37763A; padding: 3px;">оплачен';?></span></a>
+                                                <a class="table-row" style="text-decoration: underline;" href="/cabinet/orders/<?php echo $order['order_id']; ?>"> <span <?php if ($order['status'] == 0) echo 'style="background: #F3C7C7; color: #C7271F; padding: 3px;">не оплачен'; else echo 'style="background: #8FFF87; color: #37763A; padding: 3px;">оплачен';?></span></a>
                                             </td>
                                             <td style="width: 180px;">
-                                                <a class="table-row" style="text-decoration: underline;" href="/OnlineShop/cabinet/orders/<?php echo $order['order_id']; ?>"> <?php echo $order['total_price'] . ' €'; ?></a>
+                                                <a class="table-row" style="text-decoration: underline;" href="/cabinet/orders/<?php echo $order['order_id']; ?>"> <?php echo $order['total_price'] . ' €'; ?></a>
                                             </td>                                            
                                         </tr>
                                     <?php }?>                                      
